22bc630699f226457b12a2dcc7e272f169044b46
charlesbabbage0 <93190550+charlesbabbage0@users.noreply.github.com>
2021-10-27 22:26:51 +0530

    Add files via upload

---
+++ qr genarator.py
@@ -0,0 +1,12 @@
+import qrcode
+
+input_data = input("link::")
+
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
